c9f3ae61d71fecdeb431159e24acc825a7eb30e2
tanisha1527 <vermatanisha620@gmail.com>
2024-12-06 23:59:13 +0530

    Add cards to feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -60,8 +60,57 @@ const Feed = () => {
        <h3>Brocoders</h3>
        <p>15k views &bull; 2 days ago</p>
      </div>
+     <div className='card'>
+       <img src={thumbnail1} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail2} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail3} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail4} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail5} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail6} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail7} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
+     <div className='card'>
+       <img src={thumbnail8} alt="" />
+       <h2>Best channel to learn coding</h2>
+       <h3>Brocoders</h3>
+       <p>15k views &bull; 2 days ago</p>
+     </div>
     </div> 
   )
 }
 
 export default Feed
+
