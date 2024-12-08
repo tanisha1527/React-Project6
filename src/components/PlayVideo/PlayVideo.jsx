44a184f385e1ea619cab6aa8486438b3fe445cf8
tanisha1527 <vermatanisha620@gmail.com>
2024-12-08 21:44:27 +0530

    Add real-time PlayVideo title data from API

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,13 +7,26 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import megan from '../../assets/megan.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY } from '../../data'
 
 const PlayVideo = ({videoId}) => {
+
+ const [apiData,setApiData] = useState(null);
+
+ const fetchVideoData = async () =>{
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]));
+ }
+
+ useEffect(()=>{
+    fetchVideoData();
+ },[])
+
   return (
     <div className='play-video'>
        {/*<video src={video1} controls autoPlay muted></video>*/}
        <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-       <h3>Best youtube channel to learn coding</h3>
+       <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
        <div className='play-video-info'>
          <p>1432 views &bull; 3 days ago</p>
          <div>
